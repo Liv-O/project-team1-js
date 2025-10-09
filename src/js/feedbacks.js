c547640863f723f27e7750d37ffddb2a61b67a82
Eternal <gracious0603@gmail.com>
2025-10-09 21:23:13 +0200

    update_HTML_CSS

--- src/js/feedbacks.js
+++ src/js/feedbacks.js
@@ -1,15 +1,17 @@
-// Імпортуємо Swiper і потрібні модулі
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 
-// Імпортуємо стилі
+// Підключаємо базові стилі Swiper
 import 'swiper/css';
-// cd
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
 
 // Ініціалізація Swiper
 const swiper = new Swiper('.swiper-div', {
-  modules: [Navigation, Pagination], // додаємо модулі
+  modules: [Navigation, Pagination],
   loop: true,
+//   slidesPerView: 1,
+  spaceBetween: 24,
 
   pagination: {
     el: '.swiper-pagination',
@@ -22,8 +24,15 @@ const swiper = new Swiper('.swiper-div', {
     addIcons: false,
   },
 
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
   },
 });
