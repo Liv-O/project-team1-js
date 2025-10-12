f4108f316fdce72e1cdaae927503a01484058335
Roman_Lubennikov <lubennikovroman@gmail.com>
2025-10-12 20:33:33 +0300

    js+html

--- src/js/header.js
+++ src/js/header.js
@@ -5,12 +5,7 @@ const menuLinks = document.querySelectorAll('.menu-link, .header-link');
 
 const isMobile = window.matchMedia('(max-width: 768px)').matches;
 
-if (mobMenu && !mobMenu.classList.contains('hidden')) {
-  mobMenu.classList.add('hidden');
-}
-
 if (mobMenu && isMobile) {
-
   const onEscPress = (e) => {
     if (e.key === 'Escape') closeMenu();
   };
@@ -21,7 +16,6 @@ if (mobMenu && isMobile) {
 
   const onLinkClick = (e) => {
     e.preventDefault();
-
     const targetID = e.currentTarget.getAttribute('href');
     const targetSection = document.querySelector(targetID);
 
@@ -32,33 +26,29 @@ if (mobMenu && isMobile) {
       });
     }
 
-    if (e.currentTarget.classList.contains('menu-link')) {
-      closeMenu();
-    }
+    e.currentTarget.blur();
+    if (e.currentTarget.classList.contains('menu-link')) closeMenu();
   };
 
   const openMenu = () => {
-    mobMenu.classList.remove('hidden');
     mobMenu.classList.add('is-open');
     document.body.style.overflow = 'hidden';
 
     document.addEventListener('keydown', onEscPress);
-    closeBtn && closeBtn.addEventListener('click', closeMenu);
     mobMenu.addEventListener('click', onOverlayClick);
-    menuLinks.forEach(link => link.addEventListener('click', onLinkClick));
+    closeBtn?.addEventListener('click', closeMenu);
+    menuLinks.forEach((link) => link.addEventListener('click', onLinkClick));
   };
 
   const closeMenu = () => {
     mobMenu.classList.remove('is-open');
-    mobMenu.classList.add('hidden');
     document.body.style.overflow = '';
 
     document.removeEventListener('keydown', onEscPress);
-    closeBtn && closeBtn.removeEventListener('click', closeMenu);
     mobMenu.removeEventListener('click', onOverlayClick);
-    menuLinks.forEach(link => link.removeEventListener('click', onLinkClick));
+    closeBtn?.removeEventListener('click', closeMenu);
+    menuLinks.forEach((link) => link.removeEventListener('click', onLinkClick));
   };
 
-
-  menuBtn && menuBtn.addEventListener('click', openMenu);
+  menuBtn?.addEventListener('click', openMenu);
 }
